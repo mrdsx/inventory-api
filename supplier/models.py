d66f75b449894bd07e597e94ab56824b6dab424d
mrdsx <andre0.castlegames@gmail.com>
2025-08-02 19:52:58 +0300

    refactor(supplier): edit DB_Supplier model attributes

--- supplier/models.py
+++ supplier/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
 
@@ -6,6 +6,6 @@ from database import Base
 class DB_Supplier(Base):
     __tablename__ = "suppliers"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    contact_email = Column(String, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    contact_email: Mapped[str] = mapped_column(nullable=False)
