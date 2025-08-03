42e9e76dafb54507d8e89956f6e605d8d0dbd5dc
mrdsx <andre0.castlegames@gmail.com>
2025-08-03 16:35:56 +0300

    fix(supplier): edit supplier db model to fix supplier endpoints

--- supplier/models.py
+++ supplier/models.py
@@ -6,6 +6,6 @@ from database import Base
 class Supplier(Base):
     __tablename__ = "suppliers"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(primary_key=True, unique=True)
     contact_email: Mapped[str] = mapped_column(nullable=False)
