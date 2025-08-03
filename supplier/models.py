ab77ee9e0ced5a7c403fe2466eb83ac73acabfad
mrdsx <andre0.castlegames@gmail.com>
2025-08-03 15:34:11 +0300

    feat(supplier): edit constraints in supplier db object

--- supplier/models.py
+++ supplier/models.py
@@ -7,5 +7,5 @@ class Supplier(Base):
     __tablename__ = "suppliers"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(primary_key=True)
     contact_email: Mapped[str] = mapped_column(nullable=False)
