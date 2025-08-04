f93a97d95a5dd0acfb196ad4c6507f738c142153
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 13:50:40 +0300

    feat(order_item): add order existence validation in get route

--- app/order/validation.py
+++ app/order/validation.py
@@ -1,6 +1,18 @@
 from fastapi import HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from order_item.schemas import OrderItemPayload
+from .models import Order
+
+
+async def validate_order_exists(order_id: int, session: AsyncSession) -> None:
+    result = await session.execute(select(Order).where(Order.id == order_id))
+    db_order = result.scalar()
+    if db_order is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
+        )
 
 
 def validate_order_items(items: list[OrderItemPayload]) -> None:
--- app/order_item/routes.py
+++ app/order_item/routes.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import prefixes
 from database import get_db
+from order.validation import validate_order_exists
 from .models import OrderItem
 from .schemas import OrderItemSchema
 
@@ -14,6 +15,8 @@ router = APIRouter(prefix=prefixes.API_ROUTER_PREFIX)
 async def get_order_items_by_order_id(
     order_id: int, session: AsyncSession = Depends(get_db)
 ):
+    await validate_order_exists(order_id, session)
+
     result = await session.execute(
         select(OrderItem).where(OrderItem.order_id == order_id)
     )
