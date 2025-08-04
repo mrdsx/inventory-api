5b51ce8c8ded978871f00303ab9e7d3de57bda63
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 14:26:00 +0300

    feat(order): add get_order_info_by_order_id route

--- app/order/routes.py
+++ app/order/routes.py
@@ -4,13 +4,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db
 from prefixes import API_ROUTER_PREFIX
+from order_item.models import OrderItem
 from order_item.services import save_order_items
 from supplier.models import Supplier
-from supplier.services import find_supplier_by_name
+from supplier.services import find_supplier_by_id, find_supplier_by_name
 from .models import Order
 from .schemas import OrderPayload, OrderSchema
 from .services import save_order
-from .validation import validate_order_items
+from .validation import validate_order_exists, validate_order_items
 
 
 router = APIRouter(prefix=API_ROUTER_PREFIX)
@@ -28,6 +29,33 @@ async def get_orders(session: AsyncSession = Depends(get_db)):
         }
         for order, supplier in result
     ]
+
+
+# TODO: Extract repeating code
+@router.get("/orders/{order_id}")
+async def get_order_info_by_order_id(order_id: int, session: AsyncSession = Depends(get_db)):  # type: ignore
+    await validate_order_exists(order_id, session)
+
+    result = await session.execute(
+        select(OrderItem).where(OrderItem.order_id == order_id)
+    )
+
+    total_cost: float = 0
+    for order_item in result.scalars().all():
+        total_cost += order_item.cost * order_item.quantity
+
+    result2 = await session.execute(select(Order).where(Order.id == order_id))
+    order = result2.scalar()
+
+    supplier = await find_supplier_by_id(order.supplier_id, session)  # type: ignore
+
+    return {
+        "id": order.id,  # type: ignore
+        "supplier_name": supplier.name,
+        "date": order.date,  # type: ignore
+        "status": order.status,  # type: ignore
+        "total_cost": total_cost,
+    }  # type: ignore
 
 
 @router.post("/orders", response_model=OrderSchema)
