30863021c07899d0b6d2696ba61a6add46d3497a
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 16:37:56 +0300

    feat(supplier): update __init__.py in 'supplier' domain

--- app/supplier/__init__.py
+++ app/supplier/__init__.py
@@ -1,5 +1,13 @@
+from .models import Supplier
 from .routes import router as router_suppliers
+from .services import find_supplier_by_id, find_supplier_by_name
 
 __all__ = [
+    # models
+    "Supplier",
+    # routers
     "router_suppliers",
+    # services
+    "find_supplier_by_id",
+    "find_supplier_by_name",
 ]
