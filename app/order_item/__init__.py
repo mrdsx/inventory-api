be96c558774c1adfb4764f97eac539e60b31165d
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 18:04:40 +0300

    feat(order_item): add order item db model in __init__.py

--- app/order_item/__init__.py
+++ app/order_item/__init__.py
@@ -1,7 +1,10 @@
+from .models import OrderItem
 from .schemas import OrderItemPayload, OrderItemSchema
 from .services import find_order_items_by_order_id, save_order_items
 
 __all__ = [
+    # models
+    "OrderItem",
     # schemas
     "OrderItemPayload",
     "OrderItemSchema",
