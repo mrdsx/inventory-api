496ce4cb6a7c7a1a261e7093788f337a9a03b71d
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 13:50:59 +0300

    feat(order_item): add order items router in main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from product import router_products
 from supplier import router_suppliers
 from order import router_orders
+from order_item import router_order_items
 
 
 app = FastAPI()
@@ -21,6 +22,7 @@ def root():
 
 app.include_router(router_suppliers)
 app.include_router(router_products)
+app.include_router(router_order_items)
 app.include_router(router_orders)
 
 
