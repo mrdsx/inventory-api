f11a3a52eeb162efc2bb8d33d3b04670b580ca0b
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 16:23:43 +0300

    refactor: update imports from 'order' domain

--- app/order_item/services.py
+++ app/order_item/services.py
@@ -1,7 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from order.models import Order
+from order import Order
 from .models import OrderItem
 from .schemas import OrderItemPayload
 
