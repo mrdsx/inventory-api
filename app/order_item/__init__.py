97560ee6b6a06c88ac41889d70109eb4cc27bb3d
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 18:12:36 +0300

    feat(order_item): add new service fn in __init__.py

--- app/order_item/__init__.py
+++ app/order_item/__init__.py
@@ -1,6 +1,10 @@
 from .models import OrderItem
 from .schemas import OrderItemPayload, OrderItemSchema
-from .services import find_order_items_by_order_id, save_order_items
+from .services import (
+    find_order_item_by_id,
+    find_order_items_by_order_id,
+    save_order_items,
+)
 
 __all__ = [
     # models
@@ -9,6 +13,7 @@ __all__ = [
     "OrderItemPayload",
     "OrderItemSchema",
     # services
+    "find_order_item_by_id",
     "find_order_items_by_order_id",
     "save_order_items",
 ]
