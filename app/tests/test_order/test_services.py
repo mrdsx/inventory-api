c41818939be484ff99f08d3c0ed9b6544e504828
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 23:24:38 +0300

    test(order): add test for order service functions

---
+++ app/tests/test_order/test_services.py
@@ -0,0 +1,45 @@
+from unittest.mock import AsyncMock
+import pytest
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from order.constants import OrderStatuses
+from order.models import Order
+from order.services import find_order_by_id, save_order
+
+NOT_EXISTING_ORDER_ID = 1
+EXISTING_ORDER_ID = 5
+MOCK_ORDER_ID = 1
+MOCK_ORDER_SUPPLIER_ID = 123
+
+
+@pytest.mark.asyncio
+async def test_find_order_by_id(session: AsyncSession):
+    with pytest.raises(HTTPException) as exc_info:
+        await find_order_by_id(NOT_EXISTING_ORDER_ID, session)
+
+    assert exc_info.value.status_code == status.HTTP_404_NOT_FOUND
+    assert "Order not found" in str(exc_info.value.detail)
+
+    found_order = await find_order_by_id(EXISTING_ORDER_ID, session)
+    assert found_order.id == EXISTING_ORDER_ID
+    assert found_order.status == OrderStatuses.IN_TRANSIT
+
+
+@pytest.mark.asyncio
+async def test_save_order():
+    mock_session = AsyncMock(spec=AsyncSession)
+
+    mock_session.commit = AsyncMock()
+    mock_session.refresh = AsyncMock(
+        side_effect=lambda x: setattr(x, "id", MOCK_ORDER_ID)  # type: ignore
+    )
+
+    result = await save_order(supplier_id=MOCK_ORDER_SUPPLIER_ID, session=mock_session)
+
+    assert isinstance(result, Order)
+    assert result.id == MOCK_ORDER_ID
+    assert result.supplier_id == MOCK_ORDER_SUPPLIER_ID
+
+    mock_session.commit.assert_awaited_once()
+    mock_session.refresh.assert_awaited_once_with(result)
