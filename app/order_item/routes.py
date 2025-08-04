0a25191d64a1dcd7d35cb45745e17585a602b12a
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 14:49:14 +0300

    refactor(order_item): replace querying order items in get route with service fn

--- app/order_item/routes.py
+++ app/order_item/routes.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import prefixes
 from database import get_db
 from order.validation import validate_order_exists
-from .models import OrderItem
 from .schemas import OrderItemSchema
+from .services import find_order_items_by_order_id
 
 router = APIRouter(prefix=prefixes.API_ROUTER_PREFIX)
 
@@ -16,9 +15,6 @@ async def get_order_items_by_order_id(
     order_id: int, session: AsyncSession = Depends(get_db)
 ):
     await validate_order_exists(order_id, session)
+    order_items = await find_order_items_by_order_id(order_id, session)
 
-    result = await session.execute(
-        select(OrderItem).where(OrderItem.order_id == order_id)
-    )
-
-    return result.scalars().all()
+    return order_items
