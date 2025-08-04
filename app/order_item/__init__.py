82f4571f00da1903b83e22bfcb0f8ff4cfe59d45
mrdsx <andre0.castlegames@gmail.com>
2025-08-04 13:48:59 +0300

    feat(order_item): add new domain 'order_items'

---
+++ app/order_item/__init__.py
@@ -0,0 +1,3 @@
+from .routes import router as router_order_items
+
+__all__ = ["router_order_items"]
---
+++ app/order_item/routes.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import prefixes
+from database import get_db
+from .models import OrderItem
+from .schemas import OrderItemSchema
+
+router = APIRouter(prefix=prefixes.API_ROUTER_PREFIX)
+
+
+@router.get("/order_items/{order_id}", response_model=list[OrderItemSchema])
+async def get_order_items_by_order_id(
+    order_id: int, session: AsyncSession = Depends(get_db)
+):
+    result = await session.execute(
+        select(OrderItem).where(OrderItem.order_id == order_id)
+    )
+
+    return result.scalars().all()
