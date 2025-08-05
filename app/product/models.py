e9774c0d2573439f38f3858653f3cf95c02e2bf6
mrdsx <andre0.castlegames@gmail.com>
2025-08-05 17:06:13 +0300

    feat(product): edit 'supplier_id' attribute of product db object position in table

--- app/product/models.py
+++ app/product/models.py
@@ -9,11 +9,11 @@ class Product(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     sku: Mapped[str] = mapped_column(String(30), primary_key=True)
+    supplier_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("suppliers.id"), nullable=False
+    )
     name: Mapped[str] = mapped_column(String, nullable=False)
     description: Mapped[str] = mapped_column(String(1000), nullable=False)
     category: Mapped[str] = mapped_column(String, nullable=False)
     cost: Mapped[float] = mapped_column(Float, nullable=False)
     price: Mapped[float] = mapped_column(Float, nullable=False)
-    supplier_id: Mapped[int] = mapped_column(
-        Integer, ForeignKey("suppliers.id"), nullable=False
-    )
