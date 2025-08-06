11dcc860a163710cd09ee78f9f67ca78b6d3582f
mrdsx <andre0.castlegames@gmail.com>
2025-08-06 20:23:37 +0300

    test(order_item): add test for find_order_item_by_id service

---
+++ app/tests/test_order_item/test_order_item_services.py
@@ -0,0 +1,31 @@
+import pytest
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from unittest.mock import AsyncMock, MagicMock
+
+from order_item import find_order_item_by_id, OrderItem
+from tests.constants import MOCK_ORDER_ID, MOCK_ORDER_ITEM_ID
+
+
+@pytest.mark.asyncio
+async def test_find_order_item_by_id():
+    mock_session = AsyncMock(spec=AsyncSession)
+
+    # * Test for error
+    mock_result = MagicMock()
+    mock_result.scalar.return_value = None
+    mock_session.execute.return_value = mock_result
+
+    with pytest.raises(HTTPException) as exc_info:
+        await find_order_item_by_id(MOCK_ORDER_ITEM_ID, MOCK_ORDER_ID, mock_session)
+
+    assert exc_info.value.status_code == status.HTTP_404_NOT_FOUND
+    assert "Order item not found" in exc_info.value.detail
+
+    # * Test for success
+    mock_result.scalar.return_value = OrderItem(id=MOCK_ORDER_ITEM_ID)
+    order_item = await find_order_item_by_id(
+        MOCK_ORDER_ITEM_ID, MOCK_ORDER_ID, mock_session
+    )
+    assert isinstance(order_item, OrderItem)
+    assert order_item.id == MOCK_ORDER_ITEM_ID
