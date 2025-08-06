26129a71d37119d217cc665bfad075d0ee9b6001
mrdsx <andre0.castlegames@gmail.com>
2025-08-06 19:26:34 +0300

    refactor(order_item): add missing return type hints in services

--- app/order_item/services.py
+++ app/order_item/services.py
@@ -1,13 +1,16 @@
 from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Sequence
 
 from order import Order
 from .models import OrderItem
 from .schemas import OrderItemPayload
 
 
-async def find_order_item_by_id(item_id: int, order_id: int, session: AsyncSession):
+async def find_order_item_by_id(
+    item_id: int, order_id: int, session: AsyncSession
+) -> OrderItem:
     result = await session.execute(
         select(OrderItem).where(
             OrderItem.id == item_id and OrderItem.order_id == order_id
@@ -22,7 +25,9 @@ async def find_order_item_by_id(item_id: int, order_id: int, session: AsyncSessi
     return db_order_item
 
 
-async def find_order_items_by_order_id(order_id: int, session: AsyncSession):
+async def find_order_items_by_order_id(
+    order_id: int, session: AsyncSession
+) -> Sequence[OrderItem]:
     result = await session.execute(
         select(OrderItem).where(OrderItem.order_id == order_id)
     )
@@ -32,7 +37,7 @@ async def find_order_items_by_order_id(order_id: int, session: AsyncSession):
 
 async def save_order_items(
     order: Order, items: list[OrderItemPayload], session: AsyncSession
-):
+) -> None:
     order_items = [
         OrderItem(
             order_id=order.id,
