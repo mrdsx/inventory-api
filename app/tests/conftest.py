14792820b1b98346f49c2609321c2e996b4308ac
mrdsx <andre0.castlegames@gmail.com>
2025-08-06 17:30:19 +0300

    test: remove unused session in conftest.py

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,23 +1,9 @@
 import pytest
-import pytest_asyncio
 from fastapi.testclient import TestClient
 
-from database import Base, engine, SessionLocal
 from main import app
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
-
-
-@pytest_asyncio.fixture
-async def session():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        await session.close()
