e70a68eec6961ef85bee025a78044f80e3bb97e2
mrdsx <andre0.castlegames@gmail.com>
2025-08-06 20:37:02 +0300

    test(order_item): add test for save_order_items service fn

--- app/tests/test_order_item/test_order_item_services.py
+++ app/tests/test_order_item/test_order_item_services.py
@@ -3,8 +3,19 @@ from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from unittest.mock import AsyncMock, MagicMock
 
-from order_item import find_order_item_by_id, OrderItem
-from tests.constants import MOCK_ORDER_ID, MOCK_ORDER_ITEM_ID
+from order import Order
+from order_item import (
+    find_order_item_by_id,
+    OrderItem,
+    OrderItemPayload,
+    save_order_items,
+)
+from tests.constants import (
+    MOCK_ORDER_ID,
+    MOCK_ORDER_ITEM_COST,
+    MOCK_ORDER_ITEM_ID,
+    MOCK_ORDER_ITEM_QUANTITY,
+)
 
 
 @pytest.mark.asyncio
@@ -29,3 +40,21 @@ async def test_find_order_item_by_id():
     )
     assert isinstance(order_item, OrderItem)
     assert order_item.id == MOCK_ORDER_ITEM_ID
+
+
+@pytest.mark.asyncio
+async def test_save_order_items():
+    mock_session = AsyncMock(spec=AsyncSession)
+
+    mock_order = Order(id=MOCK_ORDER_ID)
+    mock_order_items = [
+        OrderItemPayload(
+            name="Order Item",
+            description="",
+            category="",
+            cost=MOCK_ORDER_ITEM_COST,
+            quantity=MOCK_ORDER_ITEM_QUANTITY,
+        )
+    ]
+
+    await save_order_items(mock_order, mock_order_items, mock_session)
