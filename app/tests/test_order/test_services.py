1671e6d4244fc11abd2ce047af3980b8b48e2267
mrdsx <andre0.castlegames@gmail.com>
2025-08-06 17:27:42 +0300

    test(order): replace db data with mock data in test_find_order_by_id, remove positional arguments

--- app/tests/test_order/test_services.py
+++ app/tests/test_order/test_services.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, MagicMock
 
 from tests.test_order.constants import (
     EXISTING_ORDER_ID,
@@ -13,14 +13,26 @@ from order import find_order_by_id, Order, OrderStatus, save_order
 
 
 @pytest.mark.asyncio
-async def test_find_order_by_id(session: AsyncSession):
+async def test_find_order_by_id():
+    mock_session = AsyncMock()
+
+    # * Test for error
+    mock_result = MagicMock()
+    mock_result.scalar.return_value = None
+    mock_session.execute.return_value = mock_result
+
     with pytest.raises(HTTPException) as exc_info:
-        await find_order_by_id(NOT_EXISTING_ORDER_ID, session)
+        await find_order_by_id(NOT_EXISTING_ORDER_ID, mock_session)
 
     assert exc_info.value.status_code == status.HTTP_404_NOT_FOUND
     assert "Order not found" in str(exc_info.value.detail)
 
-    found_order = await find_order_by_id(EXISTING_ORDER_ID, session)
+    # * Test for success
+    mock_result.scalar.return_value = Order(
+        id=EXISTING_ORDER_ID, status=OrderStatus.IN_TRANSIT
+    )
+
+    found_order = await find_order_by_id(EXISTING_ORDER_ID, mock_session)
     assert found_order.id == EXISTING_ORDER_ID
     assert found_order.status in OrderStatus
 
@@ -34,7 +46,7 @@ async def test_save_order():
         side_effect=lambda x: setattr(x, "id", MOCK_ORDER_ID)  # type: ignore
     )
 
-    result = await save_order(supplier_id=MOCK_ORDER_SUPPLIER_ID, session=mock_session)
+    result = await save_order(MOCK_ORDER_SUPPLIER_ID, mock_session)
 
     assert isinstance(result, Order)
     assert result.id == MOCK_ORDER_ID
