888edd4a02ea33816a53a411b4819eb3f7cbb5cc
mrdsx <andre0.castlegames@gmail.com>
2025-08-07 15:56:36 +0300

    fix(product): add imports from services to resolve circular import error

--- app/product/__init__.py
+++ app/product/__init__.py
@@ -1,3 +1,10 @@
+from .services import find_product_by_id, save_products
 from .routes import router as router_products
 
-__all__ = ["router_products"]
+__all__ = [
+    # routes
+    "router_products",
+    # services
+    "find_product_by_id",
+    "save_products",
+]
