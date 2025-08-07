3aa2771a0361d663824f3dc1d1e02aa7cbd7a0d1
mrdsx <andre0.castlegames@gmail.com>
2025-08-07 13:15:11 +0300

    feat(supplier): add enum for response messages

--- app/supplier/__init__.py
+++ app/supplier/__init__.py
@@ -1,8 +1,11 @@
+from .constants import ResponseMsg
 from .models import Supplier
 from .routes import router as router_suppliers
 from .services import find_supplier_by_id, find_supplier_by_name
 
 __all__ = [
+    # constants
+    "ResponseMsg",
     # models
     "Supplier",
     # routers
