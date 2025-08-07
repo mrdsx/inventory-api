4a7de0208d1a776a9e19c3b50ba894f5ec381879
mrdsx <andre0.castlegames@gmail.com>
2025-08-07 13:37:57 +0300

    fix(product): edit product schema to fix validation error in get_products route

--- app/product/schemas.py
+++ app/product/schemas.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field
 class ProductSchema(BaseModel):
     id: int
     sku: str = Field(min_length=3, max_length=30, pattern="^[A-Z0-9-]+$")
+    supplier_id: int
     name: str
     description: str = Field(max_length=1000)
     category: str
     cost: float
     price: float
-    supplier: str
