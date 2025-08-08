ded58d4e721b58de3a7bb025997212054d8ff1e2
mrdsx <andre0.castlegames@gmail.com>
2025-08-08 13:56:24 +0300

    feat: add __init__.py file with schemas

---
+++ app2/schemas/__init__.py
@@ -0,0 +1,21 @@
+from .order import CreateOrderSchema, OrderPublicSchema, OrderSchema
+from .order_item import CreateOrderItemSchema, OrderItemSchema
+from .product import CreateProductSchema, ProductSchema, UpdateProductSchema
+from .supplier import SupplierPayload, SupplierSchema
+
+__all__ = [
+    # * order
+    "CreateOrderSchema",
+    "OrderPublicSchema",
+    "OrderSchema",
+    # * order_item
+    "CreateOrderItemSchema",
+    "OrderItemSchema",
+    # * product
+    "CreateProductSchema",
+    "ProductSchema",
+    "UpdateProductSchema",
+    # * supplier
+    "SupplierPayload",
+    "SupplierSchema",
+]
