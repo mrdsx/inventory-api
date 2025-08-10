9606af51071c3420c55f8c2c4674b5248a37d04d
mrdsx <andre0.castlegames@gmail.com>
2025-08-10 16:27:59 +0300

    feat(frontend): add 'page not found' page

---
+++ frontend/src/app/global-not-found.tsx
@@ -0,0 +1,14 @@
+import "@/styles/globals.css";
+
+export default function GlobalNotFoundPage() {
+  return (
+    <html lang="en">
+      <body>
+        <div>
+          <div>Error 404</div>
+          <div>Not Found</div>
+        </div>
+      </body>
+    </html>
+  );
+}
