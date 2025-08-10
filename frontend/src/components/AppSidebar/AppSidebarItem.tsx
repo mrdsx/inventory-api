e13737b9425fa9ec35c44e40e0bb8ec4a687c98d
mrdsx <andre0.castlegames@gmail.com>
2025-08-10 16:30:05 +0300

    feat(frontend/sidebar): add highlighting active sidebar route

--- frontend/src/components/AppSidebar/AppSidebarItem.tsx
+++ frontend/src/components/AppSidebar/AppSidebarItem.tsx
@@ -1,12 +1,24 @@
 "use client";
-import { SidebarItem } from "@/features/sidebar";
+import { SidebarItem, useActiveSidebarRouteStore } from "@/features/sidebar";
 import Link from "next/link";
 import { SidebarMenuButton, SidebarMenuItem } from "../ui";
 
 export function AppSidebarItem({ item }: { item: SidebarItem }) {
+  const { activeSidebarRoute, setActiveSidebarRoute } =
+    useActiveSidebarRouteStore();
+
+  const isActive = item.link == activeSidebarRoute;
+  const className = `${
+    isActive && "bg-primary text-white hover:bg-primary/80 hover:text-white"
+  }`;
+
+  function handleClick(): void {
+    setActiveSidebarRoute(item.link);
+  }
+
   return (
     <SidebarMenuItem>
-      <SidebarMenuButton asChild>
+      <SidebarMenuButton className={className} onClick={handleClick} asChild>
         <Link href={item.link}>
           {item.icon}
           {item.title}
