584dfb78513c3fba104978c8abfcd5df90017091
mrdsx <andre0.castlegames@gmail.com>
2025-08-10 22:35:38 +0300

    feat(frontend/dashboard): add styles to make recent orders table scrollable

--- frontend/src/app/(workspace)/dashboard/components/RecentOrdersSection/RecentOrdersSection.tsx
+++ frontend/src/app/(workspace)/dashboard/components/RecentOrdersSection/RecentOrdersSection.tsx
@@ -1,32 +1,44 @@
 export async function RecentOrdersSection() {
-  const res = await fetch("http://127.0.0.1:3000/api/v1/orders");
+  const res = await fetch(
+    "http://127.0.0.1:3000/api/v1/orders?limit=10&order_by_recent=true",
+  );
   const orders = await res.json();
 
   return (
     <div className="card grid gap-2">
       <span className="text-lg">Recent Orders</span>
-      <table className="w-full">
-        <thead className="bg-neutral-300 dark:bg-neutral-700">
-          <tr>
-            <th className="p-2 text-start">ID</th>
-            <th className="text-start">Supplier Name</th>
-            <th className="text-start">Date</th>
-            <th className="text-start">Status</th>
-            <th className="text-start">Total Cost</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order) => (
-            <tr key={order.id}>
-              <th className="p-2 text-start font-normal">Order #{order.id}</th>
-              <th className="text-start font-normal">{order.supplier_name}</th>
-              <th className="text-start font-normal">{order.date}</th>
-              <th className="text-start font-normal">{order.status}</th>
-              <th className="text-start font-normal">{order.total_cost}</th>
+      <div className="max-h-60 overflow-y-auto">
+        <table className="w-full">
+          <thead className="bg-neutral-300 dark:bg-neutral-700">
+            <tr>
+              <th className="sticky top-0 bg-neutral-300 p-2 text-start">ID</th>
+              <th className="sticky top-0 bg-neutral-300 text-start">
+                Supplier Name
+              </th>
+              <th className="sticky top-0 bg-neutral-300 text-start">Date</th>
+              <th className="sticky top-0 bg-neutral-300 text-start">Status</th>
+              <th className="sticky top-0 bg-neutral-300 text-start">
+                Total Cost
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order.id}>
+                <th className="p-2 text-start font-normal">
+                  Order #{order.id}
+                </th>
+                <th className="text-start font-normal">
+                  {order.supplier_name}
+                </th>
+                <th className="text-start font-normal">{order.date}</th>
+                <th className="text-start font-normal">{order.status}</th>
+                <th className="text-start font-normal">{order.total_cost}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
