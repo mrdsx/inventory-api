1c4fff47404735a3ac0101219a1937a3c9292203
mrdsx <andre0.castlegames@gmail.com>
2025-08-11 10:41:35 +0300

    feat(frontend/order): add dynamic styles depending on order status

--- frontend/src/app/(workspace)/dashboard/components/RecentOrdersSection/OrderRow.tsx
+++ frontend/src/app/(workspace)/dashboard/components/RecentOrdersSection/OrderRow.tsx
@@ -1,12 +1,23 @@
-import { OrderResponse } from "@/features/order";
+import { ORDER_STATUS, OrderResponse } from "@/features/order";
 
 export function OrderRow({ order }: { order: OrderResponse }) {
+  const orderStatusStyles =
+    order.status === ORDER_STATUS.DELIVERED
+      ? "bg-green-500"
+      : order.status === ORDER_STATUS.CANCELED
+        ? "bg-red-500"
+        : "bg-blue-500";
+
   return (
     <tr>
       <th className="p-2 text-start font-normal">Order #{order.id}</th>
       <th className="text-start font-normal">{order.supplier_name}</th>
       <th className="text-start font-normal">{order.date}</th>
-      <th className="text-start font-normal">{order.status}</th>
+      <th className="text-start font-normal">
+        <span className={`rounded-full px-3 py-1 ${orderStatusStyles}`}>
+          {order.status}
+        </span>
+      </th>
       <th className="text-start font-normal">{order.total_cost}</th>
     </tr>
   );
