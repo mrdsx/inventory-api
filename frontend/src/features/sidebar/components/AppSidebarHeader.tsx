d48bf0e9b58b21bc415c9ddfc0ae981c9987ea9a
alt-gdsx <kingofakneser@gmail.com>
2025-08-11 18:34:38 +0300

    feat(frontend/sidebar): add highlighting active route after reloading site

--- frontend/src/features/sidebar/components/AppSidebarHeader.tsx
+++ frontend/src/features/sidebar/components/AppSidebarHeader.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { Path } from "@/app/lib";
 import { SidebarHeader, useSidebar } from "@/components/ui";
 import { Webhook } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { useActiveSidebarRouteStore } from "../store/activeSidebarRouteStore";
 
 export function AppSidebarHeader() {
+  const { setActiveSidebarRoute } = useActiveSidebarRouteStore();
+  const pathname = usePathname() as Path;
   const { state } = useSidebar();
   const isCollapsed = state === "collapsed";
+
+  useEffect(() => setActiveSidebarRoute(pathname), []);
 
   return (
     <SidebarHeader>
