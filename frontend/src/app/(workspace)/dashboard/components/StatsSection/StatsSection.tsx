1b92d19dd40994a513479c35781820fcf0e54ba3
mrdsx <andre0.castlegames@gmail.com>
2025-08-11 11:37:28 +0300

    feat(frontend/order): add fetching canceled and pending orders count in StatsSection

--- frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsSection.tsx
+++ frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsSection.tsx
@@ -5,16 +5,24 @@ import { StatsCard } from "./StatsCard";
 const statsData = [
   { title: "Total Order", value: 1000 },
   { title: "Total Sales", value: 0 },
-  { title: "Total Canceled", value: "50" },
-  { title: "Total Pending", value: "500" },
+  { title: "Total Canceled", value: 0 },
+  { title: "Total In Transit", value: 0 },
 ];
 
 export async function StatsSection() {
   const ordersCount: OrdersCountResponse = await apiClient(
     `${ENDPOINTS.orders}?count=true`,
   );
+  const canceledOrdersCount: OrdersCountResponse = await apiClient(
+    `${ENDPOINTS.orders}?count=true&status=Canceled`,
+  );
+  const pendingOrdersCount: OrdersCountResponse = await apiClient(
+    `${ENDPOINTS.orders}?count=true&status=Canceled`,
+  );
 
   statsData[0].value = ordersCount.orders_count;
+  statsData[2].value = canceledOrdersCount.orders_count;
+  statsData[3].value = pendingOrdersCount.orders_count;
 
   return (
     <div className="grid grid-cols-4 gap-4">
