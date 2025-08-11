ae049facefa0efe42ff19c2b077e2642f88f7c89
alt-gdsx <kingofakneser@gmail.com>
2025-08-11 17:23:55 +0300

    feat(frontend/order): update OrdersTableRow spacing and styles of status column

--- frontend/src/features/order/components/OrdersTableRow.tsx
+++ frontend/src/features/order/components/OrdersTableRow.tsx
@@ -4,20 +4,24 @@ import { ORDER_STATUS, OrderResponse } from "@/features/order";
 export function OrdersTableRow({ order }: { order: OrderResponse }) {
   const orderStatusStyles =
     order.status === ORDER_STATUS.DELIVERED
-      ? "text-green-500"
+      ? "text-green-800 bg-green-300/50 dark:bg-green-300/80"
       : order.status === ORDER_STATUS.CANCELED
-        ? "text-red-500"
-        : "text-blue-500";
+        ? "text-red-800 bg-red-300/50 dark:bg-red-300/80"
+        : "text-blue-800 bg-blue-300/50 dark:bg-blue-300/80";
 
   return (
     <TableRow>
-      <TableCell className="p-2 text-start font-normal">{order.id}</TableCell>
+      <TableCell className="px-2 py-3 text-start font-normal">
+        {order.id}
+      </TableCell>
       <TableCell className="w-[20%] text-start font-normal">
         {order.supplier_name}
       </TableCell>
       <TableCell className="text-start font-normal">{order.date}</TableCell>
-      <TableCell className={`text-start font-normal ${orderStatusStyles}`}>
-        {order.status}
+      <TableCell className="text-start font-normal">
+        <span className={`rounded-md px-3 py-1 ${orderStatusStyles}`}>
+          {order.status}
+        </span>
       </TableCell>
       <TableCell className="text-start font-normal">
         {order.total_cost}
