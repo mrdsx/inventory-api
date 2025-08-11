27e5ee4307bcab0e291a7114831b5de0f1e3b8a9
mrdsx <andre0.castlegames@gmail.com>
2025-08-11 11:35:22 +0300

    feat(frontend/dashboard): type stats input in StatsCard component

--- frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsCard.tsx
+++ frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsCard.tsx
@@ -1,4 +1,9 @@
-export function StatsCard({ stats }: { stats: any }) {
+type StatsItem = {
+  title: string;
+  value: string | number;
+};
+
+export function StatsCard({ stats }: { stats: StatsItem }) {
   return (
     <div className="card grid gap-2">
       <span className="text-lg text-gray-500">{stats.title}</span>
