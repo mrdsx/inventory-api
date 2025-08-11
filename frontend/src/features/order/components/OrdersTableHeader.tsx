a28c9bd571eacccb6102195dd0a4c7f9f15ca4d0
alt-gdsx <kingofakneser@gmail.com>
2025-08-11 19:10:37 +0300

    feat(frontend/order): make OrdersTableHeader sticky

--- frontend/src/features/order/components/OrdersTableHeader.tsx
+++ frontend/src/features/order/components/OrdersTableHeader.tsx
@@ -10,7 +10,7 @@ const THEAD_ITEMS = [
 
 export function OrdersTableHeader() {
   return (
-    <TableHeader>
+    <TableHeader className="sticky top-0">
       <TableRow className="bg-gray-100 hover:bg-gray-200 dark:bg-neutral-900 dark:hover:bg-neutral-800">
         {THEAD_ITEMS.map((item, index) => (
           <TableHead
