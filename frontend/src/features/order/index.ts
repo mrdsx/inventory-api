934c7f75e28030bf7c145f8af19e1cb9e5e8f06c
mrdsx <andre0.castlegames@gmail.com>
2025-08-12 11:09:01 +0300

    refactor(frontend/order): remove explicit re-exporting order components and services

--- frontend/src/features/order/index.ts
+++ frontend/src/features/order/index.ts
@@ -1,4 +1,4 @@
-export { OrdersTable, OrdersTableHeader, OrdersTableRow } from "./components";
+export * from "./components";
 export { ORDER_STATUS } from "./constants";
-export { getRecentOrders } from "./services";
+export * from "./services";
 export type { OrderResponse, OrdersCountResponse } from "./types";
