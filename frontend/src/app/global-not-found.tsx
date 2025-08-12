ede6b13da77bcf0477d31ef08d35834452d8259c
mrdsx <andre0.castlegames@gmail.com>
2025-08-12 13:18:59 +0300

    feat(frontend): update global-not-found page content

--- frontend/src/app/global-not-found.tsx
+++ frontend/src/app/global-not-found.tsx
@@ -1,12 +1,33 @@
+"use client";
+import { useThemeStore } from "@/features/theme";
+import "@/styles/app.css";
 import "@/styles/globals.css";
+import Link from "next/link";
+import { ROUTES } from "./lib";
 
 export default function GlobalNotFoundPage() {
+  const { isDarkMode } = useThemeStore();
+  // let isDarkMode;
+
   return (
     <html lang="en">
-      <body>
-        <div>
-          <div>Error 404</div>
-          <div>Not Found</div>
+      <body className={`${isDarkMode && "dark"}`}>
+        <div className="grid min-h-screen place-content-center">
+          <div className="card bg-background p-10 text-center">
+            <h1 className="mb-2 text-6xl font-extrabold text-red-500">404</h1>
+            <h2 className="text-foreground mb-4 text-2xl font-semibold">
+              Page Not Found
+            </h2>
+            <p className="text-foreground/60 mb-6">
+              Sorry, the page you are looking for does not exist.
+            </p>
+            <Link
+              href={ROUTES.workspace.dashboard}
+              className="bg-primary hover:bg-primary/80 rounded-sm px-3 py-2 text-white duration-100"
+            >
+              Go Home
+            </Link>
+          </div>
         </div>
       </body>
     </html>
