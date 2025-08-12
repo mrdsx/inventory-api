a7246a8651dab17b344237feb74f8e046f6351a1
mrdsx <andre0.castlegames@gmail.com>
2025-08-12 10:52:33 +0300

    refactor(frontend/order): replace types with api order responses with interfaces

--- frontend/src/features/order/types.ts
+++ frontend/src/features/order/types.ts
@@ -1,13 +1,13 @@
 import { ORDER_STATUS } from "./constants";
 
-export type OrderResponse = {
+export interface OrderResponse {
   id: number;
   supplier_name: string;
   date: string;
   status: ORDER_STATUS;
   total_cost: number;
-};
+}
 
-export type OrdersCountResponse = {
+export interface OrdersCountResponse {
   orders_count: number;
-};
+}
