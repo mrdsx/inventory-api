a2170006e2056aa06f2868ae12d93121fb17a007
mrdsx <andre0.castlegames@gmail.com>
2025-08-13 11:03:30 +0300

    refactor(frontend/order): replace fetching orders in useEffects with useQuery

--- frontend/src/app/(workspace)/orders/components/OrdersSection.tsx
+++ frontend/src/app/(workspace)/orders/components/OrdersSection.tsx
@@ -2,56 +2,24 @@
 
 import { DataTable } from "@/components/ui";
 import { getPaginatedOrders } from "@/features/order";
-import { PaginatedOrdersResponse } from "@/features/order/types";
 import { WorkspacePageContentLoader } from "@/features/workspace";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
 import { ordersTableColumns } from "./orders-table-columns";
 
 export function OrdersSection() {
   const params = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
   const page = Number(params.get("page") ?? "1");
   const itemsPerPage = Number(params.get("items-per-page") ?? "10");
 
-  const [paginatedOrders, setPaginatedOrders] =
-    useState<PaginatedOrdersResponse>({
-      items: [],
-      total: 0,
-      page: 0,
-      size: 0,
-      pages: 0,
-    });
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { data: paginatedOrders, isPending } = useQuery({
+    queryKey: ["orders", page],
+    queryFn: () => getPaginatedOrders(page, itemsPerPage),
+  });
 
-  useEffect(() => {
-    setIsLoading(true);
-    if (page < 1) replace(`${pathname}?page=1`);
-
-    getPaginatedOrders(page, itemsPerPage)
-      .then((paginatedOrders) => {
-        setPaginatedOrders(paginatedOrders);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
-
-  useEffect(() => {
-    setIsLoading(true);
-    if (page < 1) replace(`${pathname}?page=1`);
-
-    getPaginatedOrders(page, itemsPerPage)
-      .then((paginatedOrders) => {
-        setPaginatedOrders(paginatedOrders);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [page, itemsPerPage]);
-
-  if (isLoading) return <WorkspacePageContentLoader />;
+  if (isPending || paginatedOrders === undefined) {
+    return <WorkspacePageContentLoader />;
+  }
 
   return (
     <DataTable
