5a3b00b5c6c00d4d57b45ca9966369c2bd911d95
alt-gdsx <kingofakneser@gmail.com>
2025-08-14 14:12:06 +0300

    feat(frontend/dashboard): set fixed height for StatsCard

--- frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsCard.tsx
+++ frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsCard.tsx
@@ -2,7 +2,7 @@ import { StatisticsItem } from "@/app/lib";
 
 export function StatsCard({ stats }: { stats: StatisticsItem }) {
   return (
-    <div className="card grid gap-2">
+    <div className="card grid h-25 gap-2">
       <span className="text-lg text-gray-500">{stats.title}</span>
       <span className="text-xl font-semibold">{stats.value}</span>
     </div>
