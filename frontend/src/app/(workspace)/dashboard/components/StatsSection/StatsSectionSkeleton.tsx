e90db3cb1ee8845f52ef04f2c01b35bcd976a49c
alt-gdsx <kingofakneser@gmail.com>
2025-08-14 14:13:01 +0300

    feat(frontend/dashboard): update styles in StatsSectionSkeleton

--- frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsSectionSkeleton.tsx
+++ frontend/src/app/(workspace)/dashboard/components/StatsSection/StatsSectionSkeleton.tsx
@@ -8,7 +8,7 @@ export function StatsSectionSkeleton({
   return (
     <>
       {new Array(childrenCount).fill(null).map((_) => (
-        <div className="card grid gap-4 p-4">
+        <div className="card flex h-25 flex-col justify-evenly gap-2">
           <Skeleton className="h-4 w-30" />
           <Skeleton className="h-4 w-30" />
         </div>
