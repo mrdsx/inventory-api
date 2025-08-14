cc36f633a787d15224d653d4d53605d1e6645e8e
alt-gdsx <kingofakneser@gmail.com>
2025-08-14 15:11:07 +0300

    refactor(frontend/ui): update ui components exports in index.ts

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,58 +1,10 @@
-export { Button, buttonVariants } from "./button";
+export * from "./button";
 export * from "./data-table";
 export { Input } from "./input";
-export { ScrollArea, ScrollBar } from "./scroll-area";
+export * from "./scroll-area";
 export { Separator } from "./separator";
-export {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "./sheet";
-export {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupAction,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarInput,
-  SidebarInset,
-  SidebarMenu,
-  SidebarMenuAction,
-  SidebarMenuBadge,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSkeleton,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-  SidebarProvider,
-  SidebarRail,
-  SidebarSeparator,
-  SidebarTrigger,
-  useSidebar,
-} from "./sidebar";
+export * from "./sheet";
+export * from "./sidebar";
 export { Skeleton } from "./skeleton";
-export {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "./table";
-export {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./tooltip";
+export * from "./table";
+export * from "./tooltip";
