6ef76b9b987e85cf3eb74a2390d0821e514c8c38
mrdsx <andre0.castlegames@gmail.com>
2025-08-15 13:23:24 +0300

    refactor(frontend/sidebar): remove determination of active sidebar item using store

--- frontend/src/features/sidebar/components/AppSidebarItem.tsx
+++ frontend/src/features/sidebar/components/AppSidebarItem.tsx
@@ -1,25 +1,21 @@
 "use client";
 
 import { SidebarMenuButton, SidebarMenuItem } from "@/components/ui";
-import { SidebarItem, useActiveSidebarRouteStore } from "@/features/sidebar";
+import { SidebarItem } from "@/features/sidebar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function AppSidebarItem({ item }: { item: SidebarItem }) {
-  const { activeSidebarRoute, setActiveSidebarRoute } =
-    useActiveSidebarRouteStore();
+  const pathname = usePathname();
 
-  const isActive = item.link == activeSidebarRoute;
+  const isActive = pathname.includes(item.link);
   const className = `${
     isActive && "bg-primary text-white hover:bg-primary/80 hover:text-white"
   }`;
 
-  function handleClick(): void {
-    if (item.isWorkspaceRoute) setActiveSidebarRoute(item.link);
-  }
-
   return (
     <SidebarMenuItem>
-      <SidebarMenuButton className={className} onClick={handleClick} asChild>
+      <SidebarMenuButton className={className} asChild>
         <Link href={item.link}>
           {item.icon}
           {item.title}
