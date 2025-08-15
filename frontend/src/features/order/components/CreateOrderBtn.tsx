dea0d688312ab5952ce156b55ef7a0b3b91dfeb9
mrdsx <andre0.castlegames@gmail.com>
2025-08-15 21:50:38 +0300

    refactor(frontend/order): delete on click event in CreateOrderBtn

--- frontend/src/features/order/components/CreateOrderBtn.tsx
+++ frontend/src/features/order/components/CreateOrderBtn.tsx
@@ -5,15 +5,8 @@ import { Button } from "@/components/ui";
 import Link from "next/link";
 
 export function CreateOrderBtn() {
-  function handleClick() {
-    console.log("hi");
-  }
-
   return (
-    <Button
-      className="bg-primary hover:bg-primary/80 px-3 py-1 font-semibold text-white"
-      onClick={handleClick}
-    >
+    <Button className="bg-primary hover:bg-primary/80 px-3 py-1 font-semibold text-white">
       <Link href={ROUTES.workspace.orders.create}>Create Order</Link>
     </Button>
   );
