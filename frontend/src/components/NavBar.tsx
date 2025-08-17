33d7989d67840deddff0b2b87f6a4ca940695353
mrdsx <andre0.castlegames@gmail.com>
2025-08-17 12:09:49 +0300

    feat(frontend): edit styles in NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import { SidebarTrigger } from "./ui";
 
 export function NavBar() {
   return (
-    <div className="border-b-1 border-black p-2">
+    <div className="border-border border-b-1 p-2">
       <SidebarTrigger size={"lg"} />
     </div>
   );
