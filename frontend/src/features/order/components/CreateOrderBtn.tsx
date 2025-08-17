cdd0f556b97d5998fe38aedefd828306f5196bb6
mrdsx <andre0.castlegames@gmail.com>
2025-08-17 12:07:24 +0300

    refactor(frontend/order): edit html structure in CreateOrderBtn

--- frontend/src/features/order/components/CreateOrderBtn.tsx
+++ frontend/src/features/order/components/CreateOrderBtn.tsx
@@ -6,8 +6,10 @@ import Link from "next/link";
 
 export function CreateOrderBtn() {
   return (
-    <Button className="bg-primary hover:bg-primary/80 px-3 py-1 font-semibold text-white">
-      <Link href={ROUTES.workspace.orders.create}>Create Order</Link>
-    </Button>
+    <Link href={ROUTES.workspace.orders.create}>
+      <Button className="bg-primary hover:bg-primary/80 px-3 py-1 font-semibold text-white">
+        Create Order
+      </Button>
+    </Link>
   );
 }
