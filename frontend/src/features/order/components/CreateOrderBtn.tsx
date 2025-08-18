5cc7fa90a0f5cee1cc3768b373624d96d9dff955
alt-gdsx <kingofakneser@gmail.com>
2025-08-18 22:33:57 +0300

    feat(frontend/order): update styles in CreateOrderBtn

--- frontend/src/features/order/components/CreateOrderBtn.tsx
+++ frontend/src/features/order/components/CreateOrderBtn.tsx
@@ -7,7 +7,7 @@ export function CreateOrderBtn() {
   return (
     <NavigationBtn
       href={ROUTES.workspace.orders.create}
-      className="bg-primary hover:bg-primary/80 px-3 font-semibold"
+      className="bg-primary hover:bg-primary/80 px-3 font-semibold text-white"
     >
       Create Order
     </NavigationBtn>
