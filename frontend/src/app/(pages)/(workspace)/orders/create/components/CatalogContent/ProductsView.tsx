239a7f519df0fc2bd637000f9ffefc9da2e9eebe
alt-gdsx <kingofakneser@gmail.com>
2025-08-18 16:41:28 +0300

    feat(frontend/order): edit styles in ProductsView

--- frontend/src/app/(pages)/(workspace)/orders/create/components/CatalogContent/ProductsView.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/create/components/CatalogContent/ProductsView.tsx
@@ -30,7 +30,7 @@ export function ProductsView({ products }: { products: Product[] }) {
 function getProductViewStyles(productView: ProductView) {
   const layoutClassName =
     productView === "grid"
-      ? "grid-cols-2 sm:grid-cols-3 md:grid-cols-4"
+      ? "grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
       : "grid-cols-1";
   const cardClassName =
     productView === "grid" ? "flex-col" : "flex-row items-center";
