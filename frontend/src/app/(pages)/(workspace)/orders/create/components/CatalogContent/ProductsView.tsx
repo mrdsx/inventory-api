1cc5e7b7439e39db36d587dcb2d74b172a5230c0
alt-gdsx <kingofakneser@gmail.com>
2025-08-18 16:37:22 +0300

    refactor(frontend/order): extract ProductsView dynamic styles to helper fn

--- frontend/src/app/(pages)/(workspace)/orders/create/components/CatalogContent/ProductsView.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/create/components/CatalogContent/ProductsView.tsx
@@ -1,20 +1,19 @@
 import { Card } from "@/components/ui";
 import { CartItemActions } from "@/features/order";
-import { Product, ProductInfo, useProductViewStore } from "@/features/product";
+import {
+  Product,
+  ProductInfo,
+  ProductView,
+  useProductViewStore,
+} from "@/features/product";
 import { cn } from "@/lib/utils";
 
 export function ProductsView({ products }: { products: Product[] }) {
   const productView = useProductViewStore((state) => state.productView);
-
-  const divClassName =
-    productView === "grid"
-      ? "grid-cols-2 sm:grid-cols-3 md:grid-cols-4"
-      : "grid-cols-1";
-  const cardClassName =
-    productView === "grid" ? "flex-col" : "flex-row items-center";
+  const { cardClassName, layoutClassName } = getProductViewStyles(productView);
 
   return (
-    <div className={cn("grid gap-2", divClassName)}>
+    <div className={cn("grid gap-2", layoutClassName)}>
       {products.map((product) => (
         <Card
           key={product.id}
@@ -26,4 +25,15 @@ export function ProductsView({ products }: { products: Product[] }) {
       ))}
     </div>
   );
+}
+
+function getProductViewStyles(productView: ProductView) {
+  const layoutClassName =
+    productView === "grid"
+      ? "grid-cols-2 sm:grid-cols-3 md:grid-cols-4"
+      : "grid-cols-1";
+  const cardClassName =
+    productView === "grid" ? "flex-col" : "flex-row items-center";
+
+  return { layoutClassName, cardClassName };
 }
