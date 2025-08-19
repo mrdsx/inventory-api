e58acc7819fdab8a0954a1467f5c1e910151eba1
mrdsx <andre0.castlegames@gmail.com>
2025-08-19 22:13:45 +0300

    feat(frontend/order): add badge with current cart length
    affected: CreateOrderPageActions

--- frontend/src/app/(pages)/(workspace)/orders/create/components/CreateOrderPageActions.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/create/components/CreateOrderPageActions.tsx
@@ -1,17 +1,25 @@
+"use client";
+
 import { ROUTES } from "@/app/lib";
 import { GoBackBtn, NavigationBtn } from "@/components";
+import { useOrderCartStore } from "@/features/order";
 import { ShoppingCart } from "lucide-react";
 
 export function CreateOrderPageActions() {
+  const cart = useOrderCartStore((state) => state.cart);
+
   return (
     <div className="flex justify-between">
       <GoBackBtn href={ROUTES.workspace.orders.root} />
       <NavigationBtn
-        className="mr-4"
+        className="size relative mr-4"
         href={ROUTES.workspace.orders.cart}
         variant="ghost"
       >
-        <ShoppingCart />
+        <ShoppingCart className="size-6" />
+        <div className="bg-primary absolute top-0.5 right-0.5 flex size-4 items-center justify-center rounded-full text-xs">
+          {cart.length}
+        </div>
       </NavigationBtn>
     </div>
   );
