497019ebea5dd9d286b6923d5faa4c75ce34662d
mrdsx <andre0.castlegames@gmail.com>
2025-08-19 15:05:33 +0300

    feat(frontend/product): add missing return types in productUtils
    affected: getGroupedProducts

--- frontend/src/features/product/utils/productUtils.ts
+++ frontend/src/features/product/utils/productUtils.ts
@@ -13,7 +13,7 @@ export function getFilteredProducts(
 export function getGroupedProducts<T extends keyof Product>(
   products: Product[],
   groupBy: T,
-) {
+): Record<string, Product[]> {
   return Object.groupBy(products, (product) => product[groupBy]) as Record<
     string,
     Product[]
