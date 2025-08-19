ca9e1efaa473bd80043af8d798c7020ec45f1dd6
mrdsx <andre0.castlegames@gmail.com>
2025-08-19 22:30:14 +0300

    refactor(frontend/order): remove child components from CartItemRow
    affected: CartItemRow, CartItemsList

--- frontend/src/app/(pages)/(workspace)/orders/cart/components/CartItemRow.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/cart/components/CartItemRow.tsx
@@ -1,15 +1,3 @@
-import { CartItem, CartItemActions } from "@/features/order";
-import { CartItemDropdownMenu } from "./CartItemDropdownMenu";
-
-export function CartItemRow({ item }: { item: CartItem }) {
-  return (
-    <div className="flex items-center px-2 py-4">
-      <span className="mr-4 w-[30%]">{item.name}</span>
-      <CartItemActions item={item} />
-      <span className="ml-auto font-semibold text-gray-600 dark:text-gray-300">
-        ${(item.cost * item.count).toFixed(2)}
-      </span>
-      <CartItemDropdownMenu item={item} />
-    </div>
-  );
+export function CartItemRow({ children }: { children: React.ReactNode }) {
+  return <li className="flex items-center px-2 py-3.5">{children}</li>;
 }
--- frontend/src/app/(pages)/(workspace)/orders/cart/components/CartItemsList.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/cart/components/CartItemsList.tsx
@@ -1,4 +1,5 @@
-import { useOrderCartStore } from "@/features/order";
+import { CartItemActions, useOrderCartStore } from "@/features/order";
+import { CartItemDropdownMenu } from "./CartItemDropdownMenu";
 import { CartItemRow } from "./CartItemRow";
 
 export function CartItemsList() {
@@ -7,9 +8,14 @@ export function CartItemsList() {
   return (
     <ul className="divide-y-1">
       {cart.map((item) => (
-        <li key={item.id}>
-          <CartItemRow item={item} />
-        </li>
+        <CartItemRow key={item.id}>
+          <span className="mr-4 w-[30%]">{item.name}</span>
+          <CartItemActions item={item} />
+          <span className="ml-auto font-semibold text-gray-600 dark:text-gray-300">
+            ${(item.cost * item.count).toFixed(2)}
+          </span>
+          <CartItemDropdownMenu item={item} />
+        </CartItemRow>
       ))}
     </ul>
   );
