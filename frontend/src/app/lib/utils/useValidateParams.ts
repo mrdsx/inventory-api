5554dd1f20b04ddad2d8a9c0ee091a6ed481eb72
alt-gdsx <kingofakneser@gmail.com>
2025-08-19 12:51:10 +0300

    refactor(frontend/api): replace validateParams with useValidateParams hook

---
+++ frontend/src/app/lib/utils/useValidateParams.ts
@@ -0,0 +1,24 @@
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PAGE_SIZE,
+  PAGE_SIZES,
+  SEARCH_PARAMS_KEYS,
+  useParams,
+} from "@/app/lib";
+
+const { ITEMS_PER_PAGE, PAGE } = SEARCH_PARAMS_KEYS;
+
+export function useValidateParams() {
+  const { setParams, updatePathname } = useParams();
+
+  function validateParams(page: number, itemsPerPage: number) {
+    const isPageNumberPositive = page > 0;
+    if (!isPageNumberPositive || !PAGE_SIZES.includes(itemsPerPage)) {
+      setParams(PAGE, DEFAULT_PAGE);
+      setParams(ITEMS_PER_PAGE, DEFAULT_PAGE_SIZE);
+      updatePathname();
+    }
+  }
+
+  return validateParams;
+}
--- frontend/src/features/order/hooks/useOrdersQuery.tsx
+++ frontend/src/features/order/hooks/useOrdersQuery.tsx
@@ -6,7 +6,7 @@ import {
   QUERY_KEYS,
   SEARCH_PARAMS_KEYS,
   useParams,
-  validateParams,
+  useValidateParams,
 } from "@/app/lib";
 import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "../services";
@@ -15,6 +15,7 @@ const { ITEMS_PER_PAGE, PAGE } = SEARCH_PARAMS_KEYS;
 
 export function useGetOrdersQuery() {
   const { params } = useParams();
+  const validateParams = useValidateParams();
 
   const page = Number(params.get(PAGE) ?? DEFAULT_PAGE);
   const itemsPerPage = Number(params.get(ITEMS_PER_PAGE) ?? DEFAULT_PAGE_SIZE);
