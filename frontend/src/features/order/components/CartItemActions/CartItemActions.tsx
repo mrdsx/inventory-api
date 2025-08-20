ad8f5ff1fab9444cb38294f68f2fbaadc0399506
mrdsx <andre0.castlegames@gmail.com>
2025-08-20 19:29:15 +0300

    feat(frontend/cart): add button to add/remove product from favorite
    affected: CartItemActions

--- frontend/src/features/order/components/CartItemActions/CartItemActions.tsx
+++ frontend/src/features/order/components/CartItemActions/CartItemActions.tsx
@@ -1,23 +1,34 @@
+import { Button } from "@/components/ui";
 import { useOrderCartStore } from "@/features/order";
 import { Product } from "@/features/product";
+import { Heart } from "lucide-react";
 import { AddItemToCartBtn } from "./AddItemToCartBtn";
 import { DecrementItemCountBtn } from "./DecrementItemCountBtn";
 import { IncrementItemCountBtn } from "./IncrementItemCountBtn";
+import "./cart-action-btn.css";
 
 const MIN_COUNT_OF_CART_ITEM = 1;
 
 export function CartItemActions({ item }: { item: Product }) {
   const count = useOrderCartStore((state) => state.getItemCount(item.id));
 
-  if (count < MIN_COUNT_OF_CART_ITEM) {
-    return <AddItemToCartBtn item={item} />;
-  }
-
   return (
     <div className="flex items-center gap-1">
-      <DecrementItemCountBtn item={item} />
-      <span className="min-w-6 text-center">{count}</span>
-      <IncrementItemCountBtn item={item} />
+      {count < MIN_COUNT_OF_CART_ITEM ? (
+        <AddItemToCartBtn item={item} />
+      ) : (
+        <>
+          <DecrementItemCountBtn item={item} />
+          <span className="min-w-6 text-center">{count}</span>
+          <IncrementItemCountBtn item={item} />
+        </>
+      )}
+      <Button
+        className="ml-auto size-(--cart-action-btn-size)"
+        variant="outline"
+      >
+        <Heart />
+      </Button>
     </div>
   );
 }
