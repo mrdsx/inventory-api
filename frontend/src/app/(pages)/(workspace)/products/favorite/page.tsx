9e9a697493fdcb64a6bbcf202449b56adc4b86d3
mrdsx <andre0.castlegames@gmail.com>
2025-08-20 20:04:17 +0300

    feat(frontend/product): replace mock favorite products with data from store
    affected: FavoriteProductsPage

--- frontend/src/app/(pages)/(workspace)/products/favorite/page.tsx
+++ frontend/src/app/(pages)/(workspace)/products/favorite/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   ScrollArea,
   Table,
@@ -7,39 +9,53 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui";
+import { useFavoriteProductsStore } from "@/features/product";
 import { products } from "../../orders/create/mock-data";
 
-const favoriteProducts = products.filter((_, i) => i < 20);
-
 export default function FavoriteProductsPage() {
+  const favoriteProductIds = useFavoriteProductsStore(
+    (state) => state.favoriteProducts,
+  );
+  const favoriteProducts = products.filter((i) => favoriteProductIds.has(i.id));
+
   return (
     <div>
       <h2 className="mb-4 text-2xl">Favorite products</h2>
       <ScrollArea className="h-120 rounded-sm border">
-        <Table className="h-full rounded-sm outline-1 outline-(--border)">
+        <Table className="h-full rounded-sm">
           <TableHeader className="table-header sticky top-0">
             <TableRow>
               <TableHead className="w-1/4">Name</TableHead>
               <TableHead className="">Description</TableHead>
               <TableHead className="w-1/7">Supplier</TableHead>
               <TableHead className="w-1/7">Category</TableHead>
-              <TableHead className="w-1/8">Cost</TableHead>
+              <TableHead className="w-1/8 pr-10 text-end">Cost</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
-            {favoriteProducts.map((product) => (
-              <TableRow key={product.id}>
-                <TableCell>{product.name}</TableCell>
-                <TableCell>
-                  <p className="w-[40ch] overflow-hidden overflow-ellipsis">
-                    {product.description}
-                  </p>
+            {favoriteProducts.length === 0 ? (
+              <TableRow>
+                <TableCell className="py-10 text-center" colSpan={5}>
+                  No favorite products found
                 </TableCell>
-                <TableCell>{product.supplier}</TableCell>
-                <TableCell>{product.category}</TableCell>
-                <TableCell>{product.cost}</TableCell>
               </TableRow>
-            ))}
+            ) : (
+              favoriteProducts.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>
+                    <p className="w-[40ch] overflow-hidden overflow-ellipsis">
+                      {product.description}
+                    </p>
+                  </TableCell>
+                  <TableCell>{product.supplier}</TableCell>
+                  <TableCell>{product.category}</TableCell>
+                  <TableCell className="pr-10 text-end">
+                    {product.cost}
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
           </TableBody>
         </Table>
       </ScrollArea>
