fe39c478fb037bfb4012126b85d8bc5d5bb85eca
mrdsx <andre0.castlegames@gmail.com>
2025-08-20 14:04:54 +0300

    feat(frontend/order): add button to navigate to favorite products page
    affected: CreateOrderPageActions

--- frontend/src/app/(pages)/(workspace)/orders/create/components/CreateOrderPageActions.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/create/components/CreateOrderPageActions.tsx
@@ -3,26 +3,31 @@
 import { ROUTES } from "@/app/lib";
 import { GoBackBtn, NavigationBtn } from "@/components";
 import { useOrderCartStore } from "@/features/order";
-import { ShoppingCart } from "lucide-react";
+import { Heart, ShoppingCart } from "lucide-react";
 
 export function CreateOrderPageActions() {
   const cart = useOrderCartStore((state) => state.cart);
 
   return (
-    <div className="flex justify-between">
+    <div className="mb-1 flex justify-between">
       <GoBackBtn href={ROUTES.workspace.orders.root} />
-      <NavigationBtn
-        className="relative mr-4"
-        href={ROUTES.workspace.orders.cart}
-        variant="outline"
-      >
-        <ShoppingCart />
-        {cart.length > 0 && (
-          <div className="bg-primary absolute -top-1 -right-1 flex size-4 items-center justify-center rounded-full text-xs">
-            {cart.length}
-          </div>
-        )}
-      </NavigationBtn>
+      <div className="flex gap-2">
+        <NavigationBtn href={"/products/favorite"} variant="outline">
+          <Heart />
+        </NavigationBtn>
+        <NavigationBtn
+          className="relative mr-4"
+          href={ROUTES.workspace.orders.cart}
+          variant="outline"
+        >
+          <ShoppingCart />
+          {cart.length > 0 && (
+            <div className="bg-primary absolute -top-1 -right-1 flex size-4 items-center justify-center rounded-full text-xs">
+              {cart.length}
+            </div>
+          )}
+        </NavigationBtn>
+      </div>
     </div>
   );
 }
