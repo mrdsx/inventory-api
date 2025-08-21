c502d9f5fcb5e438f20e24c320dccd54316e76b8
mrdsx <andre0.castlegames@gmail.com>
2025-08-21 19:43:15 +0300

    feat(frontend/product): make favorite product ids store persistent
    affected: useFavoriteProductsStore

--- frontend/src/features/product/stores/favoriteProductsStore.ts
+++ frontend/src/features/product/stores/favoriteProductsStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, StorageValue } from "zustand/middleware";
 
 type FavoriteProductsState = {
   favoriteProducts: Set<number>;
@@ -6,17 +7,48 @@ type FavoriteProductsState = {
   removeFavoriteProductId: (id: number) => void;
 };
 
-export const useFavoriteProductsStore = create<FavoriteProductsState>(
-  (set, get) => ({
-    favoriteProducts: new Set<number>(),
-    addFavoriteProductId: (id: number) =>
-      set((state) => ({
-        favoriteProducts: new Set(state.favoriteProducts).add(id),
-      })),
-    removeFavoriteProductId: (id: number) => {
-      const nextFavoriteProductIds = get().favoriteProducts;
-      nextFavoriteProductIds.delete(id);
-      set({ favoriteProducts: nextFavoriteProductIds });
+export const useFavoriteProductsStore = create<FavoriteProductsState>()(
+  persist(
+    (set, get) => ({
+      favoriteProducts: new Set<number>(),
+      addFavoriteProductId: (id: number) =>
+        set((state) => ({
+          favoriteProducts: new Set(state.favoriteProducts).add(id),
+        })),
+      removeFavoriteProductId: (id: number) => {
+        const nextFavoriteProductIds = get().favoriteProducts;
+        nextFavoriteProductIds.delete(id);
+        set({ favoriteProducts: nextFavoriteProductIds });
+      },
+    }),
+    {
+      name: "favorite-products-store",
+      storage: {
+        getItem: (name) => {
+          const storedValue = localStorage.getItem(name);
+          if (storedValue === null) return null;
+
+          const parsed: StorageValue<FavoriteProductsState> =
+            JSON.parse(storedValue);
+          if (typeof parsed.state.favoriteProducts === "object") {
+            parsed.state.favoriteProducts = new Set(
+              parsed.state.favoriteProducts,
+            );
+          }
+          return parsed;
+        },
+        setItem: (name, value) => {
+          const serializableValue = { ...value };
+          if (serializableValue.state.favoriteProducts instanceof Set) {
+            // @ts-ignore
+            serializableValue.state.favoriteProducts = Array.from(
+              serializableValue.state.favoriteProducts,
+            );
+          }
+          localStorage.setItem(name, JSON.stringify(serializableValue));
+        },
+        removeItem: (name) => localStorage.removeItem(name),
+      },
     },
-  }),
+  ),
 );
