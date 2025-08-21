00faaa9aef7104a7b3fcc21f2928c19cbb6ab5e5
mrdsx <andre0.castlegames@gmail.com>
2025-08-21 22:46:42 +0300

    refactor(frontend/order): remove redundant OrdersActions component
    affected: OrdersPage
    reason: redundant abstraction

--- frontend/src/app/(pages)/(workspace)/orders/components/OrdersActions.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateOrderBtn } from "@/features/order";
-
-export function OrdersActions() {
-  return (
-    <div className="flex flex-col justify-between gap-4 sm:flex-row">
-      <CreateOrderBtn />
-    </div>
-  );
-}
