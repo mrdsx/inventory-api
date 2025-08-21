996a12163d7fd974d7fc109e8d1c7891d8f84a4b
mrdsx <andre0.castlegames@gmail.com>
2025-08-21 20:14:28 +0300

    feat(frontend/product): add dropdown menu in favorite products table
    affected: FavoriteProductsPage

--- frontend/src/app/(pages)/(workspace)/products/favorite/page.tsx
+++ frontend/src/app/(pages)/(workspace)/products/favorite/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
   ScrollArea,
   Table,
   TableBody,
@@ -9,14 +15,22 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui";
-import { useFavoriteProductsStore } from "@/features/product";
+import { Product, useFavoriteProductsStore } from "@/features/product";
+import { Ellipsis } from "lucide-react";
 import { products } from "../../orders/create/mock-data";
 
 export default function FavoriteProductsPage() {
   const favoriteProductIds = useFavoriteProductsStore(
     (state) => state.favoriteProducts,
   );
+  const removeFavoriteProductId = useFavoriteProductsStore(
+    (state) => state.removeFavoriteProductId,
+  );
   const favoriteProducts = products.filter((i) => favoriteProductIds.has(i.id));
+
+  function handleDeleteFromFavorites(product: Product) {
+    removeFavoriteProductId(product.id);
+  }
 
   return (
     <div>
@@ -25,17 +39,18 @@ export default function FavoriteProductsPage() {
         <Table className="h-full rounded-sm">
           <TableHeader className="table-header sticky top-0">
             <TableRow>
-              <TableHead className="w-1/4">Name</TableHead>
-              <TableHead className="">Description</TableHead>
-              <TableHead className="w-1/7">Supplier</TableHead>
-              <TableHead className="w-1/7">Category</TableHead>
+              <TableHead className="w-1/4">Name </TableHead>
+              <TableHead>Description </TableHead>
+              <TableHead className="w-1/7">Supplier </TableHead>
+              <TableHead className="w-1/7">Category </TableHead>
               <TableHead className="w-1/8 pr-10 text-end">Cost</TableHead>
+              <TableHead className="w-10" />
             </TableRow>
           </TableHeader>
           <TableBody>
             {favoriteProducts.length === 0 ? (
               <TableRow>
-                <TableCell className="py-10 text-center" colSpan={5}>
+                <TableCell className="py-10 text-center" colSpan={6}>
                   No favorite products found
                 </TableCell>
               </TableRow>
@@ -52,6 +67,26 @@ export default function FavoriteProductsPage() {
                   <TableCell>{product.category}</TableCell>
                   <TableCell className="pr-10 text-end">
                     {product.cost}
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild className="size-7">
+                        <Button variant="ghost">
+                          <Ellipsis className="size-3.5" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuGroup>
+                          {/* <DropdownMenuItem>Add to order</DropdownMenuItem> */}
+                          <DropdownMenuItem
+                            onClick={() => handleDeleteFromFavorites(product)}
+                            variant="destructive"
+                          >
+                            Remove from favorites
+                          </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
                   </TableCell>
                 </TableRow>
               ))
