9c53ff9e2ef726c9893a926864f51275a876cae9
mrdsx <andre0.castlegames@gmail.com>
2025-08-22 17:30:00 +0300

    refactor(frontend/order): rename module with useGetOrdersQuery hook
    affected: index.ts, useOrdersQuery (old name)

--- frontend/src/features/order/index.ts
+++ frontend/src/features/order/index.ts
@@ -1,6 +1,6 @@
 export * from "./components";
 export { ORDER_STATUS } from "./constants";
-export { useGetOrdersQuery } from "./hooks/useOrdersQuery";
+export { useGetOrdersQuery } from "./hooks/useGetOrdersQuery";
 export * from "./services";
 export { useOrderCartStore } from "./stores/orderCartStore";
 export * from "./types";
