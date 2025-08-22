4ea8ef0ddccd69a587832bf7efd9c286e80a2496
mrdsx <andre0.castlegames@gmail.com>
2025-08-22 17:26:53 +0300

    feat(frontend/supplier): add fetching suppliers on suppliers page
    affected: SuppliersPage

--- frontend/src/app/(pages)/(workspace)/suppliers/page.tsx
+++ frontend/src/app/(pages)/(workspace)/suppliers/page.tsx
@@ -1,5 +1,21 @@
+import { apiClient, PaginatedResponse } from "@/app/lib";
 import { PageHeading } from "@/components";
+import { DataTable } from "@/components/ui";
+import { SupplierResponse } from "@/features/supplier";
+import { SUPPLIERS_TABLE_COLUMNS } from "./suppliers-table-columns";
 
-export default function SuppliersPage() {
-  return <PageHeading>Suppliers</PageHeading>;
+export default async function SuppliersPage() {
+  const paginatedSuppliers =
+    await apiClient<PaginatedResponse<SupplierResponse[]>>("/suppliers");
+
+  return (
+    <>
+      <PageHeading>Suppliers</PageHeading>
+      <DataTable
+        className="h-100"
+        columns={SUPPLIERS_TABLE_COLUMNS}
+        paginationData={paginatedSuppliers}
+      />
+    </>
+  );
 }
