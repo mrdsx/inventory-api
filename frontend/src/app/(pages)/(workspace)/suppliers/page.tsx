bed243c7e388bf1209e0d2aadec6e7c414b45607
mrdsx <andre0.castlegames@gmail.com>
2025-08-26 15:34:44 +0300

    feat(frontend/supplier): add "create supplier" button
    affected: SuppliersPage

--- frontend/src/app/(pages)/(workspace)/suppliers/page.tsx
+++ frontend/src/app/(pages)/(workspace)/suppliers/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { PageHeading } from "@/components";
-import { DataTable } from "@/components/ui";
+import { Button, DataTable } from "@/components/ui";
 import { useGetSuppliersQuery } from "@/features/supplier";
 import { SUPPLIERS_TABLE_COLUMNS } from "./suppliers-table-columns";
 
@@ -11,8 +11,9 @@ export default function SuppliersPage() {
   return (
     <>
       <PageHeading>Suppliers</PageHeading>
+      <Button>Create Supplier</Button>
       <DataTable
-        className="h-100"
+        className="h-90"
         columns={SUPPLIERS_TABLE_COLUMNS}
         isLoading={isPending}
         paginationData={paginatedSuppliers}
