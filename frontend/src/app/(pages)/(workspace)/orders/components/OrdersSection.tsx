ad312149eb34ef6c60b6e59e54b50b08d642140d
mrdsx <andre0.castlegames@gmail.com>
2025-08-28 13:42:12 +0300

    refactor(frontend/order): remove redundant useGetOrdersQuery hook

--- frontend/src/app/(pages)/(workspace)/orders/components/OrdersSection.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/components/OrdersSection.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { QUERY_KEYS, usePaginatedQueryParams } from "@/app/lib";
 import { DataTable } from "@/components/ui";
-import { useGetOrdersQuery } from "@/features/order";
+import { getOrders } from "@/features/order";
+import { useQuery } from "@tanstack/react-query";
 import { ordersTableColumns } from "./orders-table-columns";
 
 export function OrdersSection() {
-  const { data: paginatedOrders, isPending } = useGetOrdersQuery();
+  const { itemsPerPage, page } = usePaginatedQueryParams();
+  const { data: paginatedOrders, isPending } = useQuery({
+    queryKey: [QUERY_KEYS.ORDERS, page, itemsPerPage],
+    queryFn: () => getOrders(page, itemsPerPage),
+    throwOnError: true,
+    retry: false,
+  });
 
   return (
     <DataTable
