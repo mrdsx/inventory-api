8dee3a0bded81629cd8876f0a69436ffce570884
mrdsx <andre0.castlegames@gmail.com>
2025-08-28 13:43:51 +0300

    refactor(frontend/supplier): remove redundant useGetSuppliersQuery hook

--- frontend/src/app/(pages)/(workspace)/suppliers/page.tsx
+++ frontend/src/app/(pages)/(workspace)/suppliers/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { ROUTES } from "@/app/lib";
+import { QUERY_KEYS, ROUTES, usePaginatedQueryParams } from "@/app/lib";
 import { NavigationBtn, PageHeading } from "@/components";
 import { DataTable } from "@/components/ui";
-import { useGetSuppliersQuery } from "@/features/supplier";
+import { getSuppliers } from "@/features/supplier";
+import { useQuery } from "@tanstack/react-query";
 import { SUPPLIERS_TABLE_COLUMNS } from "./suppliers-table-columns";
 
 export default function SuppliersPage() {
-  const { data: paginatedSuppliers, isPending } = useGetSuppliersQuery();
+  const { itemsPerPage, page } = usePaginatedQueryParams();
+
+  const { data: paginatedSuppliers, isPending } = useQuery({
+    queryKey: [QUERY_KEYS.SUPPLIERS, page, itemsPerPage],
+    queryFn: () => getSuppliers(page, itemsPerPage),
+    throwOnError: true,
+    retry: false,
+  });
 
   return (
     <>
--- frontend/src/features/supplier/index.ts
+++ frontend/src/features/supplier/index.ts
@@ -1,3 +1,2 @@
-export { useGetSuppliersQuery } from "./hooks/useGetSuppliersQuery";
 export * from "./services/supplier";
 export * from "./types";
