1b451351084e05ab78fca00768fa4417cf09571c
mrdsx <andre0.castlegames@gmail.com>
2025-08-31 21:39:17 +0300

    refactor(frontend/cart): move cart actions to separate component

---
+++ frontend/src/app/(pages)/(workspace)/orders/cart/components/CartActions.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui";
+import { useOrderCartStore } from "@/features/order";
+
+export function CartActions() {
+  const cart = useOrderCartStore((state) => state.cart);
+  if (cart.length === 0) return;
+
+  const totalCost = cart.reduce(
+    (total, item) => total + item.cost * item.count,
+    0,
+  );
+
+  function handleClick() {
+    console.log(cart);
+  }
+
+  return (
+    <div className="flex items-center justify-between px-4">
+      <Button className="text-md font-semibold" onClick={handleClick}>
+        Check Out
+      </Button>
+      <span className="text-base font-semibold">
+        Total: ${totalCost.toFixed(2)}
+      </span>
+    </div>
+  );
+}
--- frontend/src/app/(pages)/(workspace)/orders/cart/page.tsx
+++ frontend/src/app/(pages)/(workspace)/orders/cart/page.tsx
@@ -1,7 +1,7 @@
 import { GoBackBtn, PageHeading } from "@/components";
-import { Button, ScrollArea } from "@/components/ui";
+import { ScrollArea } from "@/components/ui";
+import { CartActions } from "./components/CartActions";
 import { CartItemsList } from "./components/CartItemsList";
-import { CartTotalCost } from "./components/CartTotalCost";
 
 export default function CartPage() {
   return (
@@ -12,10 +12,7 @@ export default function CartPage() {
         <ScrollArea className="mt-2 mb-4 h-80">
           <CartItemsList />
         </ScrollArea>
-        <div className="flex items-center justify-between px-4">
-          <Button className="text-md font-semibold">Check Out</Button>
-          <CartTotalCost />
-        </div>
+        <CartActions />
       </div>
     </>
   );
