75ab225efd05b99b0368b22a6d76e79321e94344
mrdsx <andre0.castlegames@gmail.com>
2025-09-04 21:37:49 +0300

    refactor(frontend/api): replace any with unknown in isBaseErrorResponse

--- frontend/src/app/lib/api/utils.ts
+++ frontend/src/app/lib/api/utils.ts
@@ -1,5 +1,12 @@
 import { BaseAPIErrorResponse } from "../types";
 
-export function isBaseErrorResponse(data: any): data is BaseAPIErrorResponse {
-  return data && typeof data.detail === "string";
+export function isBaseErrorResponse(
+  data: unknown,
+): data is BaseAPIErrorResponse {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    "detail" in data &&
+    typeof data.detail === "string"
+  );
 }
